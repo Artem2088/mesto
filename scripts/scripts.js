1a4b26196184e7627428e6d10d0e330746c979e0
Artem2088 <jablonsky.art@yndex.ru>
2022-02-26 16:11:49 +0300

    исправление ошибок

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,22 @@
-const pupup = document.querySelector('.pupup');
-const pupupOpened = document.querySelector('.pupup_opened');
-const pupupForm = document.querySelector('.pupup__form');
-const openPupupButton = document.querySelector('.profile__edit'); 
-const closePupupButton = document.querySelector('.pupup__close-icon');
+const popup = document.querySelector('.popup');
+const popupOpened = document.querySelector('.popup_opened');
+const popupForm = document.querySelector('.popup__form');
+const openpopupButton = document.querySelector('.profile__edit'); 
+const closepopupButton = document.querySelector('.popup__close-icon');
 const profileInfo = document.querySelector('.profile__info');
 const profileName = document.querySelector('.profile__name');
 const profileHobby = document.querySelector('.profile__hobby');
-const nameInput = document.querySelector('.pupup__name');
-const jobInput = document.querySelector('.pupup__hobby');
+const nameInput = document.querySelector('.popup__name');
+const jobInput = document.querySelector('.popup__hobby');
 
-openPupupButton.addEventListener('click', function() {
-    pupup.classList.add('pupup_opened');
+openpopupButton.addEventListener('click', function() {
+    popup.classList.add('popup_opened');
     nameInput.value = profileName.innerText;
     jobInput.value = profileHobby.innerText;
 });
 
-closePupupButton.addEventListener('click', function() {
-    pupup.classList.remove('pupup_opened');
+closepopupButton.addEventListener('click', function() {
+    popup.classList.remove('popup_opened');
 });
 
 function formSubmitHandler (evt) {
@@ -24,4 +24,4 @@ function formSubmitHandler (evt) {
     profileName.textContent = nameInput.value;
     profileHobby.textContent = jobInput.value;  
 }
-pupupForm.addEventListener('submit', formSubmitHandler); 
+popupForm.addEventListener('submit', formSubmitHandler); 
