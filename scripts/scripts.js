5593fa22dec0102b1e880cd9008f3f82df4b3c50
Artem2088 <jablonsky.art@yndex.ru>
2022-02-26 23:21:16 +0300

    исправление ошибок

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,26 @@
-const popup = document.querySelector('.popup');
-const popupOpened = document.querySelector('.popup_opened');
-const popupForm = document.querySelector('.popup__form');
-const openpopupButton = document.querySelector('.profile__edit'); 
-const closepopupButton = document.querySelector('.popup__close-icon');
-const profileInfo = document.querySelector('.profile__info');
-const profileName = document.querySelector('.profile__name');
-const profileHobby = document.querySelector('.profile__hobby');
-const nameInput = document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__hobby');
+const popup = document.querySelector(".popup");
+const popupOpened = document.querySelector(".popup_opened");
+const popupForm = document.querySelector(".popup__form");
+const openpopupButton = document.querySelector(".profile__edit");
+const closepopupButton = document.querySelector(".popup__close-icon");
+const profileName = document.querySelector(".profile__name");
+const profileHobby = document.querySelector(".profile__hobby");
+const nameInput = document.querySelector(".popup__input_write_name");
+const jobInput = document.querySelector(".popup__input_write_hobby");
 
-openpopupButton.addEventListener('click', function() {
-    popup.classList.add('popup_opened');
-    nameInput.value = profileName.innerText;
-    jobInput.value = profileHobby.innerText;
+openpopupButton.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.innerText;
+  jobInput.value = profileHobby.innerText;
 });
 
-closepopupButton.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
+closepopupButton.addEventListener("click", function () {
+  popup.classList.remove("popup_opened");
 });
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileHobby.textContent = jobInput.value;  
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileHobby.textContent = jobInput.value;
 }
-popupForm.addEventListener('submit', formSubmitHandler); 
+popupForm.addEventListener("submit", formSubmitHandler);
