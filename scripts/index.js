3a7e861f3d052d92cb8b47be6eff3e0d0dbc0b8d
Artem2088 <jablonsky.art@yndex.ru>
2022-03-03 12:55:23 +0300

    Исправление замечаний Iteration#3

--- scripts/index.js
+++ scripts/index.js
@@ -5,31 +5,26 @@ const openPopupButton = document.querySelector(".profile__edit");
 const closePopupButton = document.querySelector(".popup__close-icon");
 const profileName = document.querySelector(".profile__name");
 const profileHobby = document.querySelector(".profile__hobby");
-const popupInput = document.querySelector(".popup__input");
 const nameInput = document.querySelector(".popup__input_write_name");
 const jobInput = document.querySelector(".popup__input_write_hobby");
+
 /*--------------------------------Функция открытия popup----------------------*/
-function open(popup_opened) {
+function open() {
   popup.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileHobby.textContent;
-  return popup_opened;
 }
-
 openPopupButton.addEventListener("click", open);
-
 /*-----------------------Отправка данных с кнопки в popup-----------------------*/
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileHobby.textContent = jobInput.value;
 }
-
 popupForm.addEventListener("submit", formSubmitHandler);
+closePopupButton.addEventListener("click", close);
 
 /*--------------------------------Функция закрытия popup----------------------*/
-function close(popup__opened) {
+function close() {
   popup.classList.remove("popup_opened");
 }
-
-closePopupButton.addEventListener("click", close);
