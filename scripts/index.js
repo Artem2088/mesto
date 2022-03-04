7e8a2b6254360f93f19fdefaf2da3b5c6db898fe
Artem2088 <jablonsky.art@yndex.ru>
2022-03-04 10:44:54 +0300

    Исправление замечаний Iteration#4

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,32 @@
 /*-----------Объявляем переменные----------------------------------*/
-const popup = document.querySelector(".popup");
-const popupForm = document.querySelector(".popup__form");
-const openPopupButton = document.querySelector(".profile__edit");
-const closePopupButton = document.querySelector(".popup__close-icon");
-const profileName = document.querySelector(".profile__name");
-const profileHobby = document.querySelector(".profile__hobby");
-const nameInput = document.querySelector(".popup__input_write_name");
-const jobInput = document.querySelector(".popup__input_write_hobby");
+const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form');
+const openPopupButton = document.querySelector('.profile__edit');
+const closePopupButton = document.querySelector('.popup__close-icon');
+const profileName = document.querySelector('.profile__name');
+const profileHobby = document.querySelector('.profile__hobby');
+const nameInput = document.querySelector('.popup__input_write_name');
+const jobInput = document.querySelector('.popup__input_write_hobby');
 
 /*--------------------------------Функция открытия popup----------------------*/
 function open() {
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileHobby.textContent;
 }
-openPopupButton.addEventListener("click", open);
+openPopupButton.addEventListener('click', open);
 /*-----------------------Отправка данных с кнопки в popup-----------------------*/
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileHobby.textContent = jobInput.value;
+  close();
 }
-popupForm.addEventListener("submit", formSubmitHandler);
-closePopupButton.addEventListener("click", close);
+
+popupForm.addEventListener('submit', formSubmitHandler);
+closePopupButton.addEventListener('click', close);
 
 /*--------------------------------Функция закрытия popup----------------------*/
 function close() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
