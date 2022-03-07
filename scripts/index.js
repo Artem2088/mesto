95642552a5dd5a50899e75d947a73401a836442f
Artem2088 <jablonsky.art@yndex.ru>
2022-03-08 01:25:40 +0300

    добавил попапы и карточки

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,46 @@ const profileName = document.querySelector('.profile__name');
 const profileHobby = document.querySelector('.profile__hobby');
 const nameInput = document.querySelector('.popup__input_write_name');
 const jobInput = document.querySelector('.popup__input_write_hobby');
-
+const profileButton = document.querySelector('.profile__button');
+const popupCards = document.querySelector('.popup-cards');
+const popupCardsCloseButton = document.querySelector(
+  '.popup-cards__close-icon'
+);
+const elementImage = document.querySelector('.element__image');
+const elementTitle = document.querySelector('.element__title');
+const placeInput = document.querySelector('.popup__input_write_place');
+const urlInput = document.querySelector('.popup__input_write_url');
+const popupImage = document.querySelector('.popup-image');
+const popupImageCloseIcon = document.querySelector('.popup-image__close-icon');
+const elementList = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.element-template').content;
+/*----------------------массив с карточками -------------------------*/
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
 /*--------------------------------Функция открытия popup----------------------*/
 function open() {
   popup.classList.add('popup_opened');
@@ -15,18 +54,72 @@ function open() {
   jobInput.value = profileHobby.textContent;
 }
 openPopupButton.addEventListener('click', open);
-/*-----------------------Отправка данных с кнопки в popup-----------------------*/
+/*-----------------------Функция отправки данных с кнопки в popup-----------------------*/
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileHobby.textContent = jobInput.value;
   close();
 }
-
-popupForm.addEventListener('submit', formSubmitHandler);
-closePopupButton.addEventListener('click', close);
-
 /*--------------------------------Функция закрытия popup----------------------*/
 function close() {
   popup.classList.remove('popup_opened');
 }
+/*---------------------------------функция открытия попапа с добавлением карточек---------------------*/
+function openCards() {
+  popupCards.classList.add('popup_opened');
+  placeInput.value;
+  urlInput.value;
+}
+/*----------------------------закрытие попапа---------------------*/
+function closeCards() {
+  popupCards.classList.remove('popup_opened');
+}
+/*-----------------------Отправка данных с кнопки в popup-cards-----------------------*/
+function formSubmitCards(evt) {
+  evt.preventDefault();
+  elementTitle.textContent = placeInput.value;
+  elementImage.textContent = urlInput.value;
+  closeCards();
+}
+/*--------------------------------Функция вызова попапа с картинки ---------------------------*/
+function openImage() {
+  popupImage.classList.add('popup_opened');
+}
+/*-------------------------------функция закрытия попапа картинки-----------*/
+function closeImage() {
+  popupImage.classList.remove('popup_opened');
+}
+/*------------------------------------функция добавления карточек----------------*/
+
+/*function cardElementItems(item) {
+  const cardElement = elementTemplate.cloneNode(true);
+  cardElement.querySelector('.element__title').textContent = item.name;
+  cardElement.querySelector('.element__image').src = item.link;
+  elementList.append(cardElement);
+}
+initialCards.forEach(cardElementItems);*/
+
+initialCards.forEach(function (item) {
+  const cardElement = elementTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__title').textContent = item.name;
+  cardElement.querySelector('.element__image').src = item.link;
+
+  elementList.append(cardElement);
+});
+
+/*------------------------------------СОБЫТИЯ--------------------------*/
+popupForm.addEventListener('submit', formSubmitHandler);
+
+closePopupButton.addEventListener('click', close);
+/*-----------------------------открытие попапа с добавлением карточек с кнопки------------------*/
+profileButton.addEventListener('click', openCards);
+
+popupCardsCloseButton.addEventListener('click', closeCards);
+
+/*-----------------------------------вешаем событие на картинку-----------------*/
+elementImage.addEventListener('click', openImage);
+
+/*---------------------------------вешаем событие на закрытие попапа с картинкой----------------------*/
+popupImageCloseIcon.addEventListener('click', closeImage);
